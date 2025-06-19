2fb6e3748ee075e6a5b7bcf881dc6a1f639eb2b5
Jixin Wang <jixinwang08@gmail.com>
2025-06-18 22:56:09 -0500

    Initial Commit

---
+++ FAST.AI/FAST_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FAST_AIApp.swift
+//  FAST.AI
+//
+//  Created by Jerry Wang Admin on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FAST_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
